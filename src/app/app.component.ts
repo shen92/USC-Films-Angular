3e6e7c1d7d6afcdc237bc9f6cd0d0dd8cf72dd91
Yingjie Shen <shen92@wisc.edu>
2021-03-20 00:07:53 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CSCI571-HW8-Angular';
+}
