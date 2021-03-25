7c588a9364d003e9e1f65f16646ef118068c6ffc
Yingjie Shen <shen92@wisc.edu>
2021-03-25 04:21:49 -0500

    refactor

---
+++ src/app/components/poster-carousel/poster-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poster-carousel',
+  templateUrl: './poster-carousel.component.html',
+  styleUrls: ['./poster-carousel.component.css']
+})
+export class PosterCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
