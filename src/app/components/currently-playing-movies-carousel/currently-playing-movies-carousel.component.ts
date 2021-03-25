f176e172b0441ca4133c6cd83f5ba24817e347e1
Yingjie Shen <shen92@wisc.edu>
2021-03-25 02:28:17 -0500

    route highlight bug

--- src/app/components/currently-playing-movies-carousel/currently-playing-movies-carousel.component.ts
+++ src/app/components/currently-playing-movies-carousel/currently-playing-movies-carousel.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CurrentlyPlayingMoviesService } from 'src/app/services';
+import {  takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-currently-playing-movies-carousel',
   templateUrl: './currently-playing-movies-carousel.component.html',
   styleUrls: ['./currently-playing-movies-carousel.component.css']
 })
-export class CurrentlyPlayingMoviesCarouselComponent implements OnInit {
+export class CurrentlyPlayingMoviesCarouselComponent implements OnInit, OnDestroy {
   
   public images = [];
+  public titles = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private _currentlyPlayingMoviesService: CurrentlyPlayingMoviesService) { 
-
-  }
+  constructor(private currentlyPlayingMoviesService: CurrentlyPlayingMoviesService) { }
 
   ngOnInit(): void {
-    this._currentlyPlayingMoviesService.fetchCurrentPlayingMovies().subscribe((data: any) => {
-      this.images = data.data.map(element => element.poster_path);
+    this.currentlyPlayingMoviesService.fetchCurrentPlayingMovies().pipe(takeUntil(this.destroy$)).subscribe((response: any) => {
+      response.data.forEach(movie => {
+        this.images.push(movie.poster_path);
+        this.titles.push(movie.title)
+      });
     })
-    //this.images = this._currentlyPlayingMoviesService.fetchCurrentPlayingMovies();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
--- src/app/services/currently-playing-movies.service.ts
+++ src/app/services/currently-playing-movies.service.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
 
 import { BASE_URL } from './config'
 
@@ -10,7 +13,20 @@ export class CurrentlyPlayingMoviesService {
 
   constructor(private http: HttpClient) { }
 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
   fetchCurrentPlayingMovies() {
-    return this.http.get(`${BASE_URL}/currently-playing-movies`)
+    return this.http.get(`${BASE_URL}/currently-playing-movies`).pipe(retry(3), catchError(this.handleError));
   }
 }
