0c21d35278ee6e832df7611612aa6b585f58e602
Yingjie Shen <shen92@wisc.edu>
2021-03-25 21:28:34 -0500

    save

--- src/app/components/swimming-pool/swimming-pool.component.ts
+++ src/app/components/swimming-pool/swimming-pool.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-swimming-pool',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./swimming-pool.component.css']
 })
 export class SwimmingPoolComponent implements OnInit {
-
+  @Input() title: string;
+  
   constructor() { }
 
   ngOnInit(): void {
