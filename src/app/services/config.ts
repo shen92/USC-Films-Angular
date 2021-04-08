304cb3aa4ff58b82209192b897f7a430bf6f7f3b
Yingjie Shen <shen92@wisc.edu>
2021-04-08 05:01:56 -0500

    api update

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL: string = 'http://localhost:8080'
+export const BASE_URL: string = 'http://localhost:8080/api/v1'
 //export const BASE_URL: string = 'https://api-shen92-uscfilms.appspot.com'
